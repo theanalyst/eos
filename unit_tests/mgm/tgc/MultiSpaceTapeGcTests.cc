7d8edb6bea9ed748b7cdbd0c0664c12221aeb0c5
Steven Murray <Steven.Murray@cern.ch>
2019-11-15 10:40:47 +0100

    Added MultiSpaceTapeGcTests.cc

---
+++ unit_tests/mgm/tgc/MultiSpaceTapeGcTests.cc
@@ -0,0 +1,48 @@
+//------------------------------------------------------------------------------
+// File: MultiSpaceTapeGcTests.cc
+// Author: Steven Murray <smurray at cern dot ch>
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * EOS - the CERN Disk Storage System                                   *
+ * Copyright (C) 2018 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#include "mgm/tgc/DummyTapeGcMgm.hh"
+#include "mgm/tgc/MultiSpaceTapeGc.hh"
+
+#include <gtest/gtest.h>
+
+class TgcMultiSpaceTapeGcTest : public ::testing::Test {
+protected:
+
+  virtual void SetUp() {
+  }
+
+  virtual void TearDown() {
+  }
+};
+
+//------------------------------------------------------------------------------
+// Test
+//------------------------------------------------------------------------------
+TEST_F(TgcMultiSpaceTapeGcTest, Constructor)
+{
+  using namespace eos::mgm::tgc;
+
+  DummyTapeGcMgm mgm;
+  MultiSpaceTapeGc gc(mgm);
+}
