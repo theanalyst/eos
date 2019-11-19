34547303090ce7194b27c7189842a681a211f866
Steven Murray <Steven.Murray@cern.ch>
2019-11-19 14:32:34 +0100

    Improved tape GC part of NsCmd::StatSubcmd()

--- mgm/proc/admin/NsCmd.cc
+++ mgm/proc/admin/NsCmd.cc
@@ -539,28 +539,28 @@ NsCmd::StatSubcmd(const eos::console::NsProto_StatProto& stat,
       // Tape GC stats are only displayed if enabled for at least one EOS space
       const auto tgcStats = gOFS->mTapeGc->getStats();
       if(!tgcStats.empty()) {
-        oss << "ALL      tgc.stagerrms                   ";
+        oss << "ALL      tgc.stats=stagerrms             ";
         for(auto itor = tgcStats.begin(); itor != tgcStats.end(); itor++) {
           const std::string &tgcSpace = itor->first;
           const tgc::TapeGcStats &tgcSpaceStats = itor->second;
           oss << " " << tgcSpace << "=" << tgcSpaceStats.nbStagerrms;
         }
         oss << std::endl;
-        oss << "ALL      tgc.queuesize                   ";
+        oss << "ALL      tgc.stats=queuesize             ";
         for(auto itor = tgcStats.begin(); itor != tgcStats.end(); itor++) {
           const std::string &tgcSpace = itor->first;
           const tgc::TapeGcStats &tgcSpaceStats = itor->second;
           oss << " " << tgcSpace << "=" << tgcSpaceStats.lruQueueSize;
         }
         oss << std::endl;
-        oss << "ALL      tgc.freebytes                   ";
+        oss << "ALL      tgc.stats=freebytes             ";
         for(auto itor = tgcStats.begin(); itor != tgcStats.end(); itor++) {
           const std::string &tgcSpace = itor->first;
           const tgc::TapeGcStats &tgcSpaceStats = itor->second;
           oss << " " << tgcSpace << "=" << tgcSpaceStats.freeBytes;
         }
         oss << std::endl;
-        oss << "ALL      tgc.qrytimestamp                ";
+        oss << "ALL      tgc.stats=qrytimestamp          ";
         for(auto itor = tgcStats.begin(); itor != tgcStats.end(); itor++) {
           const std::string &tgcSpace = itor->first;
           const tgc::TapeGcStats &tgcSpaceStats = itor->second;
