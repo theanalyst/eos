201a4d73c71b599ec4b05832706ffdb319fce9f1
Fabio Luchetti <fabio.luchetti@cern.ch>
2020-02-17 19:41:17 +0100

    wip- CI: get rid of sparse unnecessary 'dependencies:'; where useful, use 'needs:' instead (allow DAG execution -> possibily faster pipelines)

--- gitlab-ci/before_script_docker_test.sh
+++ gitlab-ci/before_script_docker_test.sh
@@ -2,7 +2,14 @@
 
 sudo ./eos-docker/scripts/shutdown_services.sh
 ./eos-docker/scripts/remove_unused_images.sh
-docker pull gitlab-registry.cern.ch/dss/${EOS_IMG_NAME}${CI_COMMIT_TAG-$CI_PIPELINE_ID}
-#sudo ./eos-docker/scripts/start_services.sh -q -i gitlab-registry.cern.ch/dss/${EOS_IMG_NAME}${CI_COMMIT_TAG-$CI_PIPELINE_ID};
-#sudo ./eos-docker/scripts/start_services.sh -q -i gitlab-registry.cern.ch/dss/eos:${CI_COMMIT_TAG-$CI_PIPELINE_ID} -u gitlab-registry.cern.ch/dss/${UBUNTU_EOS_IMG_NAME}${CI_COMMIT_TAG-$CI_PIPELINE_ID};
-sudo ./eos-docker/scripts/start_services.sh -q -i gitlab-registry.cern.ch/dss/${EOS_IMG_NAME}${CI_COMMIT_TAG-$CI_PIPELINE_ID} -u gitlab-registry.cern.ch/dss/${CLI_EOS_IMG_NAME}${CI_COMMIT_TAG-$CI_PIPELINE_ID};
+
+# always only one between $CI_COMMIT_TAG $CI_PIPELINE_ID exists
+export IMAGE_TAG="eos:${BASETAG}${CI_COMMIT_TAG}${CI_PIPELINE_ID}";
+export CLI_IMAGE_TAG="eos:${BASETAG}${CLI_BASETAG}${CI_COMMIT_TAG}${CI_PIPELINE_ID}";
+
+docker pull gitlab-registry.cern.ch/dss/${IMAGE_TAG}
+docker pull gitlab-registry.cern.ch/dss/${CLI_IMAGE_TAG}
+sudo ./eos-docker/scripts/start_services.sh -q -i gitlab-registry.cern.ch/dss/${IMAGE_TAG} -u gitlab-registry.cern.ch/dss/${CLI_IMAGE_TAG};
+#docker pull gitlab-registry.cern.ch/dss/eos:1422185
+#docker pull gitlab-registry.cern.ch/dss/eos:ubuntu_bionic_client_1422185
+#sudo ./eos-docker/scripts/start_services.sh -q -i gitlab-registry.cern.ch/dss/eos:1422185 -u gitlab-registry.cern.ch/dss/eos:ubuntu_bionic_client_1422185;
--- gitlab-ci/before_script_k8s_test.sh
+++ gitlab-ci/before_script_k8s_test.sh
@@ -4,8 +4,11 @@ source /home/gitlab-runner/coe-cluster-config/st-gitlab-k8s-02/env_st-gitlab-k8s
 git clone https://gitlab.cern.ch/eos/eos-on-k8s.git
 export K8S_NAMESPACE=$(echo ${CI_JOB_NAME}-${CI_JOB_ID}-${CI_PIPELINE_ID} | tr _ -)
 
+# always only one between $CI_COMMIT_TAG $CI_PIPELINE_ID exists
 export IMAGE_TAG="${BASETAG}${CI_COMMIT_TAG}${CI_PIPELINE_ID}";
-export CLI_IMAGE_TAG="${BASETAG}${CLI_BASETAG}${CI_COMMIT_TAG}${CI_PIPELINE_ID}"; # always only one between $CI_COMMIT_TAG $CI_PIPELINE_ID exists
+export CLI_IMAGE_TAG="${BASETAG}${CLI_BASETAG}${CI_COMMIT_TAG}${CI_PIPELINE_ID}";
+#export IMAGE_TAG=1422185;
+#export CLI_IMAGE_TAG=ubuntu_bionic_client_1422185;
 
 if [[ "$CI_JOB_NAME" =~ "k8s_unit" ]]; then
     ./eos-on-k8s/create-mgm.sh -i ${IMAGE_TAG} -n ${K8S_NAMESPACE} -q;
