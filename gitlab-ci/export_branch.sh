d2f828333b6d45174bfee22b034641f3b1fc5026
Fabio Luchetti <fabio.luchetti@cern.ch>
2020-07-09 00:17:49 +0200

    CI: Restore debian packages missing signature; Fix and simplify utility "export_branch.sh"

--- gitlab-ci/export_branch.sh
+++ gitlab-ci/export_branch.sh
@@ -3,13 +3,7 @@
 #Gitlab CI enabled branches should be listed here
 
 if [[ "${CI_BUILD_REF_NAME}" == "beryl_aquamarine" ]] || [[ ${CI_BUILD_REF_NAME} == 0.3* ]] ; then
-  REFNAME="aquamarine"
+  export BRANCH="aquamarine"
 else
-  if [[ "${CI_JOB_STAGE}" == "publish" ]]; then
-    REFNAME="citrine-xdc"
-  else
-    REFNAME="citrine"
-  fi
+  export BRANCH="citrine"
 fi
-
-export BRANCH=$REFNAME
