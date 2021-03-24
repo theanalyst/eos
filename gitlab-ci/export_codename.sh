cb56be5a86c4f6b77996b89efdc22668793ee168
Fabio Luchetti <fabio.luchetti@cern.ch>
2021-03-24 17:46:54 +0100

    CI: use global CODENAME var

--- gitlab-ci/export_codename.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Gitlab CI enabled branches should be listed here
-
-if [[ "${CI_COMMIT_REF_NAME}" == "citrine" ]] || [[ ${CI_COMMIT_REF_NAME} == 4.* ]] ; then
-  export CODENAME="citrine" # @note not supported anymore
-else
-  export CODENAME="diopside"
-fi
