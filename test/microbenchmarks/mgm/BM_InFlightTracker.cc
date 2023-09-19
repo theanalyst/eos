ffcf23a618be4744a26bad8163b290d9bd760f46
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2023-09-19 11:20:08 +0200

    TEST: microbenchmarks: add more tests to InFlightTracker

--- test/microbenchmarks/mgm/BM_InFlightTracker.cc
+++ test/microbenchmarks/mgm/BM_InFlightTracker.cc
@@ -31,11 +31,37 @@ static void BM_InFlightTrackerCreateMT(benchmark::State& state)
     }
   }
   state.counters["frequency"] = benchmark::Counter(state.iterations(),
-                                                   benchmark::Counter::kIsRate);qq
+                                                   benchmark::Counter::kIsRate);
   state.counters["per_thread"] = benchmark::Counter(state.iterations(),
                                                    benchmark::Counter::kAvgThreadsRate);
 }
 
+static void BM_InFlightTrackerStallMT(benchmark::State& state)
+{
+  eos::mgm::InFlightTracker tracker;
+  eos::common::VirtualIdentity vid;
+
+  for (auto _: state) {
+    for (auto i = 0; i < state.range(0); ++i) {
+      uid_t uid  = i & 2147483647UL;
+      vid.uid = uid;
+      vid.gid = uid;
+      bool sat;
+      size_t thread_count=1;
+      eos::mgm::InFlightRegistration registerer(tracker,vid);
+      benchmark::DoNotOptimize(registerer.IsOK());
+      benchmark::DoNotOptimize(tracker.ShouldStall(uid, sat,thread_count));
+      benchmark::ClobberMemory();
+
+    }
+  }
+  state.counters["frequency"] = benchmark::Counter(state.iterations(),
+                                                   benchmark::Counter::kIsRate);
+  state.counters["per_thread"] = benchmark::Counter(state.iterations(),
+                                                    benchmark::Counter::kAvgThreadsRate);
+}
+
 BENCHMARK(BM_InFlightTrackerCreate);
 BENCHMARK(BM_InFlightTrackerCreateMT)->Range(1,512)->ThreadRange(1, 256)->UseRealTime();
+BENCHMARK(BM_InFlightTrackerStallMT)->Range(1,512)->ThreadRange(1, 256)->UseRealTime();
 BENCHMARK_MAIN();
